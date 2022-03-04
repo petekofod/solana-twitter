be75bd8066a2aa77d6191888686a571747d082ba
Pietro Kuvut <pete.kofod@protonmail.com>
2022-03-03 19:28:38 -0500

    DevNet Public

--- app/src/composables/useWorkspace.js
+++ app/src/composables/useWorkspace.js
@@ -4,8 +4,9 @@ import { Connection } from '@solana/web3.js'
 import { PublicKey } from '@solana/web3.js'
 import { Provider } from '@project-serum/anchor'
 import { Program } from '@project-serum/anchor'
-import idl from '../../../target/idl/solana_twitter.json'
+import idl from '@/idl/solana_twitter.json'
 
+const clusterUrl = process.env.VUE_APP_CLUSTER_URL
 const preflightCommitment = 'processed'
 const commitment = 'processed'
 const programID = new PublicKey(idl.metadata.address)
@@ -15,7 +16,7 @@ export const useWorkspace = () => workspace
 
 export const initWorkspace = () => {
     const wallet = useAnchorWallet()
-    const connection = new Connection('http://127.0.0.1:8899', commitment)
+    const connection = new Connection(clusterUrl, commitment)
     const provider = computed(() => new Provider(connection, wallet.value, { preflightCommitment, commitment }))
     const program = computed(() => new Program(idl, programID, provider.value))
 
